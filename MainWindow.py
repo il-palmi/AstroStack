f7ae2ee3f38d31cd13974d48bdd95de4c1fdf171
il-palmi <il-palmi@users.noreply.github.com>
2025-03-27 22:47:28 +0100

    Implemented image preview with fill button.

--- MainWindow.py
+++ MainWindow.py
@@ -1,18 +1,34 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from typing import cast
 
 class MainWindow(Gtk.Application):
     def __init__(self):
         super().__init__()
-        # GLib.set_application_name("AstroStack")
         builder = Gtk.Builder()
         builder.add_from_file("ui/mainWindow.glade")
 
-        self.window = builder.get_object("mainWindow")
+        # Main window
+        self.window = cast(Gtk.Window, builder.get_object("mainWindow"))
         self.window.connect("destroy", Gtk.main_quit)
+        self.window.set_default_size(400, 300)
+        self.window.set_resizable(True)
 
+        # Containers
+        self.mainBox = cast(Gtk.Box, builder.get_object("mainBox"))
+        self.toolBox = cast(Gtk.Box, builder.get_object("toolBox"))
+
+        # Header bar
         self.openPictureButton = builder.get_object("openPictureButton")
+        self.homeButton = cast(Gtk.Button, builder.get_object("homeButton"))
+        self.previewFillButton = cast(Gtk.Button, builder.get_object("previewFillButton"))
 
+        # Image viewer
+        self.previewScrolledWindow = cast(Gtk.ScrolledWindow, builder.get_object("previewScrolledWindow"))
+        self.previewViewport = cast(Gtk.Viewport, builder.get_object("previewViewport"))
+        self.previewImage = cast(Gtk.Image, builder.get_object("previewImage"))
+        self.originalPreviewPixbuf = None
 
+        # Init window
         self.window.show_all()
